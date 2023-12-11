c5f42eca05424b3abc5c576fd8d2fd226d68a38a
mahian maksud <mahian.maksud@fdmgroup.com>
2023-12-11 15:04:01 +0800

    implemented more models into controller

--- src/main/java/com/exercise/art/controller/InformationController.java
+++ src/main/java/com/exercise/art/controller/InformationController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.exercise.art.model.Artwork;
+import com.exercise.art.model.Transaction;
 import com.exercise.art.service.ArtworkService;
+import com.exercise.art.service.TransactionService;
 
 @Controller
 public class InformationController {
@@ -16,11 +18,38 @@ public class InformationController {
 	@Autowired
 	private ArtworkService artworkService;
 	
+	@Autowired
+	private TransactionService transactionService;
+	
 	@GetMapping("/summaryPage")
 	public String summary(Model model) {
 		
 		List<Artwork> davidArtTotal = artworkService.findByArtistName("David");
 		model.addAttribute("davidArtTotal", davidArtTotal.size());
+		
+		List<Artwork> frankArtTotal = artworkService.findByArtistName("Frank");
+		model.addAttribute("frankArtTotal", frankArtTotal.size());
+		
+		List<Artwork> jamesArtTotal = artworkService.findByArtistName("James");
+		model.addAttribute("jamesArtTotal", jamesArtTotal.size());
+		
+		List<Artwork> lynnArtTotal = artworkService.findByArtistName("Lynn");
+		model.addAttribute("lynnArtTotal", lynnArtTotal.size());
+		
+		List<Artwork> samArtTotal = artworkService.findByArtistName("Sam");
+		model.addAttribute("samArtTotal", samArtTotal.size());
+		
+		List<Transaction> dec9Sold = transactionService.findByDate("09Dec");
+		model.addAttribute("dec9Sold", dec9Sold.size());
+		
+		List<Transaction> dec10Sold = transactionService.findByDate("10Dec");
+		model.addAttribute("dec10Sold", dec10Sold.size());
+		
+		List<Transaction> dec11Sold = transactionService.findByDate("11Dec");
+		model.addAttribute("dec11Sold", dec11Sold.size());
+		
+		List<Transaction> david9Sold = transactionService.findByArtistNameAndDate("Daved", "09Dec");
+		model.addAttribute("david9Sold", david9Sold.size());
 		
 		return "summaryPage";
 	}
--- src/main/java/com/exercise/art/repository/TransactionRepo.java
+++ src/main/java/com/exercise/art/repository/TransactionRepo.java
@@ -1,11 +1,19 @@
 package com.exercise.art.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.exercise.art.model.Transaction;
 
 @Repository
-public interface TransactionRepo extends JpaRepository<Transaction, Long>{
+public interface TransactionRepo extends JpaRepository<Transaction, Long> {
+
+	public List<Transaction> findByDate(String date);
+
+	@Query("SELECT Transaction FROM Transaction t LEFT JOIN Artwork a ON t.artwork.artworkId = a.artworkId LEFT JOIN Artist r ON a.artist.name = r.name WHERE r.name = :name AND t.date = :date")
+	public List<Transaction> findByArtistNameAndDate(String name, String date);
 
 }
